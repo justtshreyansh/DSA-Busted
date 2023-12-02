93ccc53bb01f89dfe50ed3c2d911d791ea583186
Shreyansh <shreyanshmarshal2@gmail.com>
2023-12-02 11:55:42 +0530

    Reverse Integer

---
+++ Bitwise Operation/reverseInteger.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int reverse(int n){
+    int ans = 0;
+    while(n!=0){
+        int digit = n%10;
+        if((ans>INT_MAX/10)|| (ans<INT_MIN/10)){
+            return 0;
+        }
+        ans = ans*10 + digit;
+        n = n/10;
+    }
+    return ans;
+}
+int main(){ 
+   cout<<reverse(123);
+return 0;
+}
